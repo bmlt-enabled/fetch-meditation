baec3c5e083a52eca953be767d044cabbdcbd1d0
Patrick Joyce <pjaudiomv@gmail.com>
2023-10-26 11:46:23 -0400

    add swedish and french

--- src/FetchMeditation/JFT.php
+++ src/FetchMeditation/JFT.php
@@ -37,9 +37,11 @@ class JFT
         return match ($this->language) {
             'en' => $this->getEnglish(),
             'es' => $this->getSpanish(),
+            'fr' => $this->getFrench(),
             'pt' => $this->getPortuguese(),
             'it' => $this->getItalian(),
             'ru' => $this->getRussian(),
+            'sv' => $this->getSwedish(),
             default => [],
         };
     }
@@ -322,6 +324,123 @@ class JFT
         }
         $result['copyright'] = '';
         return $result;
+    }
+
+    private function getSwedish(): array
+    {
+        libxml_use_internal_errors(true);
+        $data = $this->httpGet('https://www.nasverige.org/dagens-text/');
+        $doc = new \DOMDocument();
+        $doc->loadHTML('<?xml encoding="UTF-8">' . $data);
+        libxml_clear_errors();
+        libxml_use_internal_errors(false);
+        $xpath = new \DOMXpath($doc);
+
+        $content = [
+            'date' => '',
+            'quote' => '',
+            'source' => '',
+            'thought' => '',
+            'content' => [],
+            'title' => '',
+            'page' => '',
+            'copyright' => 'Copyright (c) ' . date("Y") . ', NA World Service, Inc. All Rights Reserved',
+        ];
+
+        // Extract the date
+        $dateElement = $xpath->query('//div[@class="border-bottom mb-4"]/p[@class="h3"]');
+        if ($dateElement->length > 0) {
+            $content['date'] = trim($dateElement->item(0)->textContent);
+        }
+
+        // Extract the title
+        $dateElement = $xpath->query('//div[@class="border-bottom mb-4"]/h2');
+        if ($dateElement->length > 0) {
+            $content['title'] = trim($dateElement->item(0)->textContent);
+        }
+
+        // Extract the quote and source
+        $quoteElement = $xpath->query('//p[@class="bg-lightBlue p-4 preamble"]');
+        if ($quoteElement->length > 0) {
+            $content['quote'] = $quoteElement->item(0)->textContent;
+            $content['quote'] = preg_replace('/\s+/', ' ', $content['quote']);
+            $quoteParts = explode('/', $content['quote']);
+            $content['quote'] = trim($quoteParts[0]);
+            $content['source'] = trim(end($quoteParts));
+        }
+
+        // Extract the thought
+        $thoughtElement = $xpath->query('//div[@class="col-12 col-md-8 col-lg-6 pt-5"]/p[2]');
+        if ($thoughtElement->length > 0) {
+            $content['thought'] = trim($thoughtElement->item(0)->textContent);
+        }
+
+        // Extract the content
+        $contentElement = $xpath->query('//div[@class="col-12 col-md-8 col-lg-6 pt-5"]/p[1]');
+        if ($thoughtElement->length > 0) {
+            $items = explode("\n", trim($contentElement->item(0)->textContent));
+            $content['content'] = array_filter($items, 'strlen');
+        }
+        return $content;
+    }
+
+    private function getFrench(): array
+    {
+        libxml_use_internal_errors(true);
+        $data = $this->httpGet('https://jpa.narcotiquesanonymes.org/');
+        $doc = new \DOMDocument();
+        $doc->loadHTML('<?xml encoding="UTF-8">' . $data);
+        libxml_clear_errors();
+        libxml_use_internal_errors(false);
+        $xpath = new \DOMXpath($doc);
+
+        $content = [
+            'date' => '',
+            'quote' => '',
+            'source' => '',
+            'thought' => '',
+            'content' => [],
+            'title' => '',
+            'page' => '',
+            'copyright' => 'Copyright (c) ' . date("Y") . ', NA World Service, Inc. All Rights Reserved',
+        ];
+
+        // Extract the title
+        $titleElement = $xpath->query('//div[@class="cartouche"]/h1');
+        if ($titleElement->length > 0) {
+            $content['title'] = trim($titleElement->item(0)->textContent);
+        }
+
+        // Extract the date
+        $dateElement = $xpath->query('//p[@class="info-publi"]');
+        if ($dateElement->length > 0) {
+            $content['date'] = trim($dateElement->item(0)->textContent);
+        }
+
+        // Extract the quote and source
+        $quoteDiv = $xpath->query('//div[@class="chapo"]')->item(0);
+        $quotePTags = $quoteDiv->getElementsByTagName('p');
+        if ($quotePTags->length >= 2) {
+            $content['quote'] = trim($quotePTags->item(0)->textContent);
+            $content['source'] = trim($quotePTags->item(1)->textContent);
+        }
+
+        // Extract the thought
+        $thoughtElement = $xpath->query('//h3[@class="h3 spip"]');
+        if ($thoughtElement->length > 0) {
+            $content['thought'] = trim($thoughtElement->item(0)->textContent);
+        }
+
+        // Extract the content
+        $textDiv = $xpath->query('//div[@class="texte"]');
+        if ($textDiv->length > 0) {
+            $content['content'] = [];
+            $pTags = $textDiv->item(0)->getElementsByTagName('p');
+            foreach ($pTags as $pTag) {
+                $content['content'][] = trim($pTag->textContent);
+            }
+        }
+        return $content;
     }
 
     private static function getTimezoneDate(string $language, $format = 'md'): string
